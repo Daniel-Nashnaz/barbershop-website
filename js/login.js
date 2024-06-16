fe9240d788123caedb2427b6747cd960574c64e1
Daniel Nassi <daniel.40768@gmail.com>
2024-06-17 01:12:23 +0300

    [IMPROVE] login

--- js/login.js
+++ js/login.js
@@ -1,48 +1,46 @@
+import { IP, PORT } from "./constants.js";
 import { validateEmail } from "./validation.js";
+let myTimeout;
 
-document.getElementById('loginForm').addEventListener('submit', async(event) => {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+    const errorMessage = document.getElementById('error');
+    const selectedEmail = document.getElementById('email');
 
-    const formData = new FormData(event.target);
-    const email = formData.get('email');
-    console.log(email);
+    selectedEmail.addEventListener('input', () => {
+        errorMessage.innerHTML = '';
+    });
 
-    const validationResult = validateEmail(email);
-    if (!validationResult === "") {
-        console.error(validationResult);
-        return;
-    }
-    document.getElementById('loading').style.display = 'block';
-    document.getElementById('loginContainer').style.display = 'none';
+    document.getElementById('loginForm').addEventListener('submit', async(event) => {
+        event.preventDefault();
+        try {
+            const formData = new FormData(event.target);
+            const email = formData.get('email');
+            console.log(email);
 
-    const myTimeout = setTimeout(myGreeting, 5000);
+            const validationResult = validateEmail(email);
+            if (!validationResult === "") {
+                console.error(validationResult);
+                return;
+            }
+            document.getElementById('loginContainer').style.display = 'none';
+            document.getElementById('loading').style.display = 'block';
 
-    function myGreeting() {
-        document.getElementById('loginContainer').style.display = 'block';
+            myTimeout = setTimeout(() => {
+                document.getElementById('loading').style.display = 'none';
+                document.getElementById('loginContainer').style.display = 'block';
+            }, 5000);
 
-        document.getElementById('loading').style.display = 'none';
-        window.location.href = 'index.html';
-    }
+            const response = await fetch(`http://${IP}:${PORT}/getUseroleData/${email}`);
+            const userData = await response.json();
+            localStorage.setItem('userData', JSON.stringify(userData));
+            console.log(userData);
 
+            window.location.href = 'userInfo.html';
 
-    //  const response = await fetch(`/getAppointments?email=${email}`);
-    // const appointments = await response.json();
-    console.log(email);
-    // שמירת פרטי המשתמש ב־LocalStorage
-    localStorage.setItem('userEmail', email);
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            errorMessage.innerHTML = 'Error fetching user data try again';
+        }
 
-    // שמירת הפגישות ב־LocalStorage
-    //localStorage.setItem('userAppointments', JSON.stringify(appointments));
-
-    // מעבר לדף הבא
-
-
-    // const appointmentsDiv = document.getElementById('appointments');
-    // appointmentsDiv.innerHTML = ''; // נוקה את התוכן הקודם של הפגישות
-
-    // appointments.forEach(appointment => {
-    //     const appointmentElement = document.createElement('div');
-    //     appointmentElement.textContent = `Date: ${appointment.date}, Time: ${appointment.time}`;
-    //     appointmentsDiv.appendChild(appointmentElement);
-    // });
+    });
 });
